f462fa2e6296f1389bcac9b167a15f8ce93bb52e
JaredDC <jared.dc@outlook.com>
2021-06-28 20:27:59 +0800

    FFFFFFFFFFFFFFFF

--- DailyWallpaper/fuck-appveyor.bat
+++ DailyWallpaper/fuck-appveyor.bat
@@ -1,10 +1,10 @@
 REM cd to project dir.
 REM FUCK
 echo "bin\Release\"
-RM /Q /S "..\Output"
-md "..\Output"
-copy "bin\Release\"  "..\Output" /y
-del "..\Output\config*.ini" 
-del "..\Output\*.log.txt"
+RM /Q /S "..\OUTPUT"
+md "..\OUTPUT"
+copy "bin\Release\"  "..\OUTPUT" /y
+del "..\OUTPUT\config*.ini" 
+del "..\OUTPUT\*.log.txt"
 echo fuck appveyor.
 set errorlevel=0
