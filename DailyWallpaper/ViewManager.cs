d163436f5841c9fa4076ed37e4eebd05200decc9
JaredDC <jared.dc@outlook.com>
2021-06-30 02:21:36 +0800

    remove ugly wpflib

--- DailyWallpaper/ViewManager.cs
+++ DailyWallpaper/ViewManager.cs
@@ -19,16 +19,8 @@ namespace DailyWallpaper
 
              var notifyIconHelper = NotifyIconManager.GetInstance();
             _notifyIcon = notifyIconHelper.notifyIcon;
-
-            _aboutViewModel = new DailyWallpaperWpfLib.ViewModel.AboutViewModel();
-            _statusViewModel = new DailyWallpaperWpfLib.ViewModel.StatusViewModel();
-
-            _statusViewModel.Icon = AppIcon;
-            _aboutViewModel.Icon = _statusViewModel.Icon;
-
             _hiddenWindow = new System.Windows.Window();
             _hiddenWindow.Hide();
-            
         }
 
         System.Windows.Media.ImageSource AppIcon
@@ -49,32 +41,6 @@ namespace DailyWallpaper
         
         // The Windows system tray class
         private NotifyIcon _notifyIcon;  
-        private DailyWallpaperWpfLib.View.AboutView _aboutView;
-        private DailyWallpaperWpfLib.ViewModel.AboutViewModel _aboutViewModel;
-        private DailyWallpaperWpfLib.ViewModel.StatusViewModel _statusViewModel;
-
-        private void ShowAboutView()
-        {
-            if (_aboutView == null)
-            {
-                _aboutView = new DailyWallpaperWpfLib.View.AboutView();
-                _aboutView.DataContext = _aboutViewModel;
-                _aboutView.Closing += ((arg_1, arg_2) => _aboutView = null);
-                _aboutView.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-
-                _aboutView.Show();
-            }
-            else
-            {
-                _aboutView.Activate();
-            }
-            _aboutView.Icon = AppIcon;
-
-            _aboutViewModel.AddVersionInfo("Hardware", "DeviceName");
-            _aboutViewModel.AddVersionInfo("Version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
-            _aboutViewModel.AddVersionInfo("Serial Number", "142573462354");
-        }
-
 
         protected override void Dispose(bool disposing)
         {
