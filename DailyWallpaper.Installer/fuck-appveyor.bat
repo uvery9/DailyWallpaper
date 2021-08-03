7c8f5da2fdd15edfe5406b449548c19b2ed4f8bb
JaredDC <jared.dc@outlook.com>
2021-08-03 14:54:34 +0800

    del old ini and pdb file.

--- DailyWallpaper.Installer/fuck-appveyor.bat
+++ DailyWallpaper.Installer/fuck-appveyor.bat
@@ -4,7 +4,8 @@ ECHO COULD dir
 RMDIR /Q /S "..\OUTPUT"
 md "..\OUTPUT"
 copy "..\DailyWallpaper\bin\Release\"  "..\OUTPUT" /y
-del "..\OUTPUT\config*.ini" 
+del "..\OUTPUT\*.config.*" 
+del "..\OUTPUT\DailyWallpaper.pdb" 
 del "..\OUTPUT\*.log.txt"
 echo fuck appveyor: NO XCOPY.
 set errorlevel=0
