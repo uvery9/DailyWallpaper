38932b3fb519a633af46c8737299e07a34b5d576
HDC <uvery6@outlook.com>
2022-02-18 11:39:01 +0800

    update CommonCommands

--- DailyWallpaper/Tools/CommonCMDForm.cs
+++ DailyWallpaper/Tools/CommonCMDForm.cs
@@ -45,9 +45,9 @@ namespace DailyWallpaper.Tools
             AppendText(5, "taskkill /f /im DailyWall*   /t", "Kill the process");
             AppendText(6, "ping -n 4 127.0.0.1 > nul", "delay in .bat");
             AppendText(7, "Slmgr –dli", "Check if Your Windows License is Retail, OEM, or Volume");
-            AppendText(8, "\"C:\\Program Files\\Git\\usr\\bin\\grep.exe\" -i -I -r 'SOMEWORD' .", "grep find STRING recursively");
-            AppendText(9, "\"C:\\Program Files\\Git\\usr\\bin\\du.exe\" -sh $FOLDER", "Find total size of a directory");
-            AppendText(10, "\"C:\\Program Files\\Git\\usr\\bin\\du.exe\" -sh *", "Find total size of each item in a directory");
+            AppendText(8, "\"C:\\Program Files\\Git\\usr\\bin\\du.exe\" -sh $FOLDER", "Find total size of a directory");
+            AppendText(9, "\"C:\\Program Files\\Git\\usr\\bin\\du.exe\" -sh *", "Find total size of each item in a directory");
+            AppendText(10, "\"C:\\Program Files\\Git\\usr\\bin\\grep.exe\" -i -I -r 'SOMEWORD' .\r\n   grep -i -I -r 'SOMEWORD' . ", "grep find STRING recursively");
             cmdTextBox.AppendText(Environment.NewLine + Environment.NewLine  + " ### Record Common Commands to the file by yourself: " + 
                 Environment.NewLine + "    " + cmdFile);
             File.WriteAllText(cmdFile, cmdTextBox.Text, Encoding.UTF8);
